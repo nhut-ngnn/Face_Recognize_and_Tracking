80247e627a4935262cc5d0e0b74abda1c75436b7
Nhut Minh Nguyen <138198155+nhut-ngnn@users.noreply.github.com>
2024-07-07 13:07:28 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -17,20 +17,24 @@ detector = Face_Detection()
 recog = Face_Recognition()
 recog.fit_model("FaceDatabase")
 
-tracker = DeepSort(max_age=20, n_init=3, nms_max_overlap=1.0, max_iou_distance=0.7)
+tracker = DeepSort(max_age=20, n_init=1, nms_max_overlap=1.0, max_iou_distance=0.7)
 
 if __name__ == "__main__":
 
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture("C:/Users/admin/Downloads/GreyD_test.mp4")  #Add path of video
     ret, frame = cam.read()
     detector.set_frame_len(frame)
 
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (frame.shape[1], frame.shape[0]))
+    
     while(True):
         ret, frame = cam.read() 
         frame = cv2.flip(frame, 1)
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = detector.detectMultiScale(gray_frame)
-        re = recog.recognize(gray_frame, faces)
+        re, acc = recog.recognize(gray_frame, faces)
+        print(acc)
         tracks = tracker.update_tracks(re, frame=frame)
         
         for track in tracks:
@@ -41,7 +45,7 @@ if __name__ == "__main__":
                 x1, y1, x2, y2 = map(int, ltrb)
                 
                 profile = getProfile(class_id)
-                if profile is not None:
+                if profile != None:
                     label = "{}: {}".format(profile[0], profile[1])
                 else:
                     label = "{}".format(class_id)
@@ -49,7 +53,8 @@ if __name__ == "__main__":
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.rectangle(frame, (x1 - 1, y1 - 20), (x1 + len(label) * 12, y1), (0, 255, 0), -1)
                 cv2.putText(frame, label, (x1 + 5, y1 - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
+        
+        out.write(frame)
         cv2.imshow("Camera Feed", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'): 
             break
